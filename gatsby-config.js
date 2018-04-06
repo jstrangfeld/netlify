3dbf483321878629fb1f8e244434b214eb1fa504
jstrangfeld <jesse.strangfeld@gmail.com>
2018-04-06 06:22:13 +0100

    Added markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'markdown-pages',
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
+}
